8bbff74d78c1264636089676491aa3635c7d2abc
Eduardo Neves <neveseduardoti@gmail.com>
2021-10-21 13:34:04 -0300

    Página not found estilização

--- src/views/Error/NotFound.vue
+++ src/views/Error/NotFound.vue
@@ -1,5 +1,20 @@
 <template>
-    <h1>Oops! A página não foi encontrada.</h1>
+    <div class='notfound-container'>
+        <div class='notfound-wrapper'>
+            <h1>Oops! A página não foi encontrada.</h1>
+
+            <img
+                :src="require('@/assets/images/404.png')"
+                alt=""
+                title="NOTFOUND_ERROR"
+            >
+
+            <a
+                href='#'
+                @click.stop.prevent="redirect({ name: 'Home' })"
+            >Voltar para a página principal.</a>
+        </div>
+    </div>
 </template>
 
 <script>
